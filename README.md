a3352bebbac13481c855f7b008411feb50330e6e
ManuCas <manucastle@hotmail.com>
2019-05-21 11:36:53 -0500

    Manager view, view Lower Stock with table completed.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # online-store-cli
-Online store like CLI app  with Node.js and SQL database
+On line store like CLI app  with Node.js and SQL database
 
 
 
@@ -7,40 +7,34 @@ Online store like CLI app  with Node.js and SQL database
 
 
 
-COMMITS
+## COMMITS
 
-Commit 1:
+**Commit 1: **
+Initial commit, repository created.
 
-Initial commit, repo created.
-
-Commit 2:
-
+**Commit 2: **
 Package.json file created.
 
-Commit 3:
-
+**Commit 3: **
 Package.json file updated with inquirer and mysql npm packages dependencies.
 
-Commit 4:
-
+**Commit 4: **
 mysql Data Base initial scripts created.
 
-Commit 6:
-
+**Commit 6: **
 Data base connection established successfully.
 
-Commit 7:
-
+**Commit 7: **
 Query to read products  Db table tested. missing fields to print out on console.
 
-Commit 8:
-
+**Commit 8: ** 
 Added function to type product id and quantity to purchase. Prints out total and refresh data base. Readme file update.
 
-Commit 9:
-
+**Commit 9: **
 Render products data as a table added. Starting with selectable options in Manager view.
 
--**Commit:10**-
+**Commit 10: **
+Manager view, view products completed with table.
 
-Manager view view products completed whit table.
+**Commit 11: **
+Manager view, view Lower Stock with table completed.
--- bamazonManager.js
+++ bamazonManager.js
@@ -18,10 +18,8 @@ dbCon.connect(function (err) {
     if (err) throw err;
     //This line to verify DB connection
     //console.log("connected as id " + dbCon.threadId + "\n");
-
     start();
 });
-
 
 
 function start() {
@@ -38,7 +36,7 @@ function start() {
                 viewProd();
                 break;
             case op2:
-                console.log("op2");
+                viewLow();
                 break;
             case op3:
                 console.log("op3");
@@ -64,7 +62,56 @@ function viewProd() {
         // Table creation
         let config,
             output;
+        config = {
+            columns: {
+                0: {
+                    alignment: "left",
+                    width: 5,
+                },
+                1: {
+                    alignment: 'left',
+                    width: 25,
+                    wrapWord: true
+                },
+                2: {
+                    alignment: 'left',
+                    width: 25,
+                    wrapWord: true
+                },
+                3: {
+                    alignment: 'center',
+                    width: 9,
+                },
+                4: {
+                    alignment: "left",
+                    width: 6,
+                },
+            },
+        };
+        output = table(data, config);
+        console.log(output);
+        //process.stdout.write(output);
+        // End Table
+    });
+}
 
+function viewLow() {
+    
+    var query = "SELECT * from products WHERE stock_quantity < 5 ";
+    dbCon.query(query, 
+    //     [
+    //     { stock_quantity: 5 }
+    // ], 
+    function (err,res) {
+        if (err) throw err;
+        let data = [["id", "Department", "Description", "Price", "Stock"]];
+        for (var i = 0; i < res.length; i++) {
+            //create matrix with DB info
+            data.push([res[i].id, res[i].department_name, res[i].product_name, res[i].price, res[i].stock_quantity]);
+        }
+        // Table creation
+        let config,
+            output;
         config = {
             columns: {
                 0: {
